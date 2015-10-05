b762e0ba3527c8e6e7b652f0afcbb64c5cd44888
inigo gonzalez <igponce@gmail.com>
2015-10-05 13:20:02 +0200

    Script de configuración para la VM de scraping

---
+++ Azure/setup.sh
@@ -0,0 +1,19 @@
+setup.sh
+
+echo << EOF
+
+Este script instala automaticamente en la raspberry las dependencias
+necesarias para controlar los relés a través de los puertos GPIO
+y para hablar con el servidor de AZURE.
+
+Necesita privilegios de root (!!!)
+
+EOF
+
+read
+
+# Dependencias:
+# python-dev
+# picke
+
+# sudo apt-get install python-dev python-pickle
