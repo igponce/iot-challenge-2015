59f888ed580c1c602f24237d52fab5bd2c597f4d
inigo gonzalez <igponce@gmail.com>
2015-10-09 00:24:31 +0200

    Inicio de carga de precios. Espero que Pickle se porte bien entre plataformas. Si no -> JSON

--- Raspberry/webcontrol.py
+++ Raspberry/webcontrol.py
@@ -5,26 +5,47 @@ Controla los reles conectados a la raspberry desde la web
 utilizando el control manual desde un navegador, y/o
 la programacion obtenida desde Red Electrica.
 
-Copyright (C) 2013 Iñigo Gonzalez <igponce (at) gmail (dot) something>
+Copyright (C) 2015 Iñigo Gonzalez Ponce <igponce (at) gmail (dot) youknowwhat>
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+Permission is hereby granted, free of charge, to any person obtaining 
+a copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation 
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ and/or sell copies of the Software, and to permit persons to whom the Software
+ is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included 
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+DEALINGS IN THE SOFTWARE
 """
+
 import sys
 import http.server
 import picoweb
 
 DEFAULT_PORT = 8080
+FICHERO_DATOS = '../Azure/schedule.pickle'
+
+precio = {}
 
 class EnergyAppWebserver(picoweb.picoWeb):
 
-	def handle_GET_status(self):
-		return "{'ok': 'everything works okay' }"
+    def handle_GET_status(self):
+        return "{'ok': 'everything works okay' }"
 
-	def handle_GET_cost(self):
-		return '{"yyyy-mm-dd": [ 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24] }'
+    def handle_GET_cost(self):
+        return '{"yyyy-mm-dd": [ 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24] }'
 
+
+def cargaDatos (fichero)
+    fp = open ( FICHERO_DATOS)
 
 if __name__ == "__main__":
     # get the port
