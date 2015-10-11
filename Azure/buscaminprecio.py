b86252330fa5fe82250ce4f2c5ddfe39919834ff
inigo gonzalez <igponce@gmail.com>
2015-10-11 22:58:06 +0200

    Bugfix: costePorDuracion[tarifa][0] refleja el coste en esa hora y no esta a cero.

--- Azure/buscaminprecio.py
+++ Azure/buscaminprecio.py
@@ -1,47 +1,64 @@
 import pickle
+from itertools import repeat
 
 fp = open('schedule.pickle', 'rb')
 precio = pickle.load(fp)
 fp.close()
-"""
-mininterval = []
 
-for dia in precio.keys():
-	for tarifa in precio[dia]:
-		# Deberia ser 0..23
-		for h_ini in range(0,len(precio[dia][tarifa])-1):
-			h_end = min(h_ini+4, 23)
-			for intervalo in range(h_ini, h_end):
-				# agrupamos de 1 a 12 h
-				print ("h_ini: {} -> {} (intervalo={} , hend={}, min({},{})={})".format(h_ini, intervalo,intervalo,h_end, h_ini+4, 23, min(h_ini+4, 23)))
+# Creamos dos listas de precios para cada uno de los intervalos
+# costePorHora[tarifa][dia][h_ini][duracion_en_horas] = euros_por_megawatt
+# costePorIntervalo[tarifa][dia][duracion_en_horas][h_inicio] = euros_por_megawatt
 
-"""
+todos_costes     = [ 0. for x in range(0,12) ]
+# costePorHora     = { "2.0.DHA": list( repeat( [] ,24)) }
+# costePorDuracion = { "2.0.DHA": list(repeat( [], 24)) }
 
-# Crear lista de precios para cada uno de los intervalos
-# precio[horas_encendido][(h_ini,h_fin)] = euros_por_megawatt
-
-todos_costes = [0 for x in range(0,12)]
+costePorHora     = { "2.0.DHA": [ [0.] * 24 for i in range(24) ] }
+costePorDuracion = { "2.0.DHA": [ [0.] * 24 for i in range(24) ] }
 
 for dia in precio.keys():
 	tarifa = precio[dia]["2.0.DHA"]
-	# Calculamos de 1 a 10 horas
-	for interval in range(1,10):
+
+	costePorDuracion["2.0.DHA"][1] = [ tarifa[hh] for hh in range(0,23) ]
+
+	for hh in range(0,23):
+		costePorHora["2.0.DHA"][hh][1] = tarifa[hh]
+
+	for interval in range(0,10):
 		todos_costes[interval] = {}
-		for i in range(0, 24-interval):
-			acum = tarifa[i]
-			for j in range(i,i+interval):
+
+		for hora in range(0, 24-interval):
+			acum = tarifa[hora]
+			for j in range(hora,hora+interval):
 				acum += tarifa[j]
-			todos_costes[interval]["{},{}".format(i,i+interval)] = acum
+			todos_costes[interval]["{},{}".format(hora,hora+interval)] = acum
+			costePorHora["2.0.DHA"][hora][interval] = acum
+			costePorDuracion["2.0.DHA"][interval][hora] = acum
+
+# Obtenemos Lista de horas con menos coste para una duracion determinada:
+# minCoste[duracion] = ( hora, coste )
+
+minCoste = []
+for dur in range(1,10):
+	minCoste[dur] = 0
+
+
+
+
+# TODO
+#      - Ordenar el array
+#      - Devolver un array de estructuras { inicio: x , fin: x, coste } ordenadas de menor a mayor.
 
 import json
-
 print("PRECIOS ENERGIA:")
-
 print( json.dumps(precio, indent=4, sort_keys=True)  )
+# print("\n\nSUMA COSTES")
+# print( json.dumps(todos_costes, indent=4, sort_keys=True) )
+print("\n\nCostePorHoras")
+print( json.dumps(costePorHora, indent=4, sort_keys=False))
+print("\n\nCostePorDuracion")
+print( json.dumps(costePorDuracion, indent=4, sort_keys=False))
 
-print("\n\nSUMA COSTES")
+print("\n\n\n**********************\n")
+print(costePorDuracion)
 
-print( json.dumps(todos_costes, indent=4, sort_keys=True))
-
-
-print(todos_costes)
