164e40c4bd1bac387c809d5a49dec92d0fd34ce8
inigo gonzalez <igponce@gmail.com>
2015-10-12 16:37:56 +0200

    Procesamos todas las tarifas disponibles en el dia

--- Azure/buscaminprecio.py
+++ Azure/buscaminprecio.py
@@ -1,5 +1,6 @@
 import pickle
 from itertools import repeat
+import pprint
 
 fp = open('schedule.pickle', 'rb')
 precio = pickle.load(fp)
@@ -17,26 +18,21 @@ costePorHora     = { "2.0.DHA": [ [float('inf')] * 24 for i in range(24) ] }
 costePorDuracion = { "2.0.DHA": [ [float('inf')] * 24 for i in range(24) ] }
 
 for dia in precio.keys():
-	tarifa = precio[dia]["2.0.DHA"]
 
-	"""
-	costePorDuracion["2.0.DHA"][1] = [ tarifa[hh] for hh in range(0,23) ]
+	for tarifa in precio[dia].keys():
 
-	for hh in range(0,23):
-		costePorHora["2.0.DHA"][hh][1] = tarifa[hh]
+		tarifa = precio[dia]["2.0.DHA"]
 
-	"""
+		for interval in range(0,6):
+			todos_costes[interval] = {}
 
-	for interval in range(0,6):
-		todos_costes[interval] = {}
-
-		for hora in range(0, 24-interval):
-			acum = tarifa[hora]
-			for j in range(hora,hora+interval):
-				acum += tarifa[j]
-			todos_costes[interval]["{},{}".format(hora,hora+interval)] = acum
-			costePorHora["2.0.DHA"][hora][interval] = acum
-			costePorDuracion["2.0.DHA"][interval][hora] = acum
+			for hora in range(0, 24-interval):
+				acum = tarifa[hora]
+				for j in range(hora,hora+interval):
+					acum += tarifa[j]
+				todos_costes[interval]["{},{}".format(hora,hora+interval)] = acum
+				costePorHora["2.0.DHA"][hora][interval] = acum
+				costePorDuracion["2.0.DHA"][interval][hora] = acum
 
 # Obtenemos Lista de horas con menos coste para una duracion determinada:
 # minCoste[duracion] = ( hora, coste )
@@ -57,14 +53,9 @@ print (minCoste)
 
 import json
 print("PRECIOS ENERGIA:")
-print( json.dumps(precio, indent=4, sort_keys=True)  )
+pprint.pprint( json.dumps(precio, indent=4, sort_keys=True)  )
 # print("\n\nSUMA COSTES")
 # print( json.dumps(todos_costes, indent=4, sort_keys=True) )
-print("\n\nCostePorHoras")
-print( json.dumps(costePorHora, indent=4, sort_keys=False))
-print("\n\nCostePorDuracion")
-print( json.dumps(costePorDuracion, indent=4, sort_keys=False))
 
-print("\n\n\n**********************\n")
-print(costePorDuracion)
-
+print("Precios minimos por duracion")
+pprint.pprint(minCoste)
